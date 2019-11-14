8a56f0ca8de36e5392b2a482db8e226ad4617259
Annemarie van Ingen <annvaningen@gmail.com>
2019-11-14 19:16:35 +0100

    only deletes a dose and routes to destroy does not work yet

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,47 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+# starting from here on with ingredients
+
+puts 'Clearing db ingredients'
+Ingredient.destroy_all
+
+puts 'creating db ingredients'
+10.times do
+  ingredient = Ingredient.new(
+    name: Faker::Food.ingredient
+  )
+  ingredient.save
+end
+
+puts 'done with ingredients'
+
+# starting from here on with cocktails
+
+puts 'Clearing db cocktails'
+Cocktail.destroy_all
+
+puts 'Creating db cocktails'
+10.times do
+  cocktail = Cocktail.new(
+    name: Faker::Food.fruits
+  )
+  cocktail.save
+end
+
+puts 'done with cocktails'
+
+# starting from here on with doses
+
+puts 'Clearing db doses'
+Dose.destroy_all
+
+puts 'Creating db doses'
+10.times do
+  dose = Dose.new(
+    description: Faker::Food.measurement
+  )
+ dose.save
+ puts dose.description
+end
+
+puts 'done with doses'
